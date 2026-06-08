39a9dce99e0fa89a58714eb2202ae7715dc815a3
John Dykstra <john.dykstra1@gmail.com>
2026-06-08 14:50:36 -0500

    chore: add Meson packaging for v1.6

---
+++ build_jar.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -eu
+
+SRCDIR=$1
+BUILDDIR=$2
+OUTPUT_JAR=$3
+MAIN_CLASS=$4
+
+mkdir -p "$BUILDDIR/classes"
+rm -rf "$BUILDDIR/classes"/*
+
+javac -d "$BUILDDIR/classes" "$SRCDIR"/*.java
+
+cat > "$BUILDDIR/manifest.mf" <<EOF
+Manifest-Version: 1.0
+Main-Class: $MAIN_CLASS
+EOF
+
+jar cfm "$BUILDDIR/$OUTPUT_JAR" "$BUILDDIR/manifest.mf" \
+  -C "$BUILDDIR/classes" . \
+  -C "$SRCDIR" Icons
+
+cp "$BUILDDIR/$OUTPUT_JAR" "$SRCDIR/../$OUTPUT_JAR"
+
+rm -f "$BUILDDIR/manifest.mf"
+rm -rf "$BUILDDIR/classes"
